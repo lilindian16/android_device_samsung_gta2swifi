741e97beaa810a9a3154b19cbdf31b7b3e270c26
lilindian16 <jaimesequeira14@gmail.com>
2019-06-19 09:30:00 +1200

    edited boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,14 +39,15 @@ TARGET_USES_64_BIT_BINDER := true
 
 #TODO
 # Kernel
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80008000
+#BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := rolex_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8917
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1FF8000 --tags_offset 0x1DF8000
+TARGET_KERNEL_CONFIG := gta2swifi_sea_open_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
@@ -93,9 +94,8 @@ USE_XML_AUDIO_POLICY_CONF := 1
 TARGET_BOOTLOADER_BOARD_NAME := MSM8937
 TARGET_NO_BOOTLOADER := true
 
-#TODO add bluetooth folder
 # Bluetooth
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
@@ -199,21 +199,21 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 #TODO
 # Partitions
-#BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
-#BOARD_USERDATAIMAGE_PARTITION_SIZE := 25765043200 # 25765059584 - 16384
-#BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-#BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-#BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-#BLOCK_BASED_OTA := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3674210304
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11358154752
+BOARD_CACHEIMAGE_PARTITION_SIZE := 314572800
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BLOCK_BASED_OTA := true
 
 # Peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
 #TODO is this necessary
 # Properties
-TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
+#TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Power
 TARGET_HAS_LEGACY_POWER_STATS := true
